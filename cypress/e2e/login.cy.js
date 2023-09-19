27b54de31c87b0b0b1eee6923963dc905d1e9969
Estêfenis <lopefl89@gmail.com>
2023-09-19 14:31:45 -0300

    add login cypress test

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,11 @@
+describe('Login', () => {
+
+  const userEmail = Cypress.env('USER_EMAIL')
+  const userPassword = Cypress.env('USER_PASSWORD')
+
+  it('successfully log in', () => {
+    cy.login(userEmail, userPassword)
+    cy.contains('h1', 'Your Notes').should('be.visible')
+    cy.contains('a', 'Create a new note').should('be.visible')
+  })
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,32 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+//===>Signup<====
+Cypress.Commands.add('fillSignupFormAndSubmit', (email, password) => {
+  cy.intercept('GET', '**/notes').as('getNotes')
+  cy.visit('/signup')
+  cy.get('#email').type(email)
+  cy.get('#password').type(password, { log: false })
+  cy.get('#confirmPassword').type(password, { log: false })
+  cy.contains('button', 'Signup').click()
+  cy.get('#confirmationCode').should('be.visible')
+
+  cy.mailosaurGetMessage(Cypress.env('MAILOSAUR_SERVER_ID'), {
+    sentTo: email
+  }).then(message => {
+    const confirmationCode = message.html.body.match(/\d{6}/)[0]
+    cy.get('#confirmationCode').type(`${confirmationCode}{enter}`)
+    cy.wait('@getNotes')
+  })
+})
+
+//===>Login<====
+Cypress.Commands.add('login', (email, password) => {
+  cy.intercept('GET', '**/notes').as('getNotes')
+  cy.visit('/login')
+  cy.get('#email').type(email)
+  cy.get('#password').type(password, { log: false })
+  cy.contains('button', 'Login').click()
+  cy.wait('@getNotes')
+})
